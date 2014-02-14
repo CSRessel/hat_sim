20a359a56dac35fa76d7b9118c9a6939ca5b7315
Clifford Ressel <cressel@andover.edu>
2014-02-13 22:59:33 -0500

    finished static pages and title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    # TODO: put in a final name
+    base_title = "Hat Simulator Helper"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 HatSim::Application.routes.draw do
+
+  # The static pages:
+  root 'static_pages#home'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/faq',     to: 'static_pages#faq',     via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
