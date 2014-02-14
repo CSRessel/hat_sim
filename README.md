137ab2b15006d9e974ac27d5fb8c568d949631a7
Clifford Ressel <cressel@andover.edu>
2014-02-13 21:18:39 -0500

    initial commit with README

---
+++ README.md
@@ -0,0 +1,6 @@
+Overview
+========
+Final project for COMP 630; a server matching website for TF2 players.
+
+- Ruby version: 2.1.0
+- Rails version: 4.0.2
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HatSim::Application.config.session_store :cookie_store, key: '_hat_sim_session'
