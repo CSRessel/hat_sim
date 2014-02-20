cf3a4d01896f6a80b65f1ad47da7c9accde8a511
Tadeas Nemec <tnemec@polymatheia.localdomain>
2014-02-19 19:57:32 -0500

    git suuuuuuuuux also seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Server.create!(address: '1.2.3.4:5', map: 'ctf_example', password: 'examplepassword', dedicated: false, players: 3, maxplayers: 30, name: 'server 1', region: 'EST')
-Server.create!(address: '3.1.4.1:5', map: 'achievemenent_idle', password: 'examplepassword', dedicated: false, players: 8, maxplayers: 30, name: 'server 2', region: 'MST')
-Server.create!(address: '2.5.5.2:19', map: 'gah_maps', password: 'examplepassword', dedicated: true, players: 39, maxplayers: 109, name: 'server 3', region: 'PST', game: '6v6')
-Server.create!(address: '2.6.5.2:29', map: 'cp_dustbowl', password: 'examplepassword', dedicated:true, players: 5, maxplayers: 24, name: 'server 4', region: 'EST', game: 'highlander')
+# SERVERS
 
-User.create!(username: 'admin1', email: 'cressel@andover.edu', steam_link: 'clyphord', region: 'EST', admin: true, password: 'password');
+Server.create(name: 'Engineer's RP Server - PST', address: '138.8.81.149:65', map: 'pl_hydro', password: 'pa55w0rd', dedicated: 'false', game: 'unspecified', players: '15', maxplayers '15', region: 'PST')
+Server.create(name: 'Super Fun Server - EST', address: '38.130.70.206:158', map: 'ph_hydro', password: 'pa55word1234', dedicated: 'false', game: 'unspecified', players: '8', maxplayers '12', region: 'EST')
+Server.create(name: 'lowgrav100%crits RP Server - CST', address: '197.222.66.91:233', map: 'mvm_dustbowl', password: 'password1234', dedicated: 'false', game: 'unspecified', players: '6', maxplayers '29', region: 'CST')
+Server.create(name: 'Carl's Prophunt Server - CST', address: '168.231.125.193:123', map: 'ph_pipeline', password: 'pasphrase', dedicated: 'false', game: 'unspecified', players: '10', maxplayers '22', region: 'CST')
+Server.create(name: 'Carl's Prophunt Server - PST', address: '26.148.159.192:250', map: 'arena_idle', password: 'pasword', dedicated: 'false', game: 'unspecified', players: '24', maxplayers '29', region: 'PST')
+Server.create(name: 'lowgrav100%crits Prophunt Server - PST', address: '18.30.172.160:33', map: 'arena_de_dust', password: 'pessvort', dedicated: 'false', game: 'unspecified', players: '9', maxplayers '12', region: 'PST')
+Server.create(name: 'Super Fun Server - PST', address: '99.71.29.81:179', map: 'plr_dustbowl', password: 'passwerd', dedicated: 'false', game: 'unspecified', players: '11', maxplayers '11', region: 'PST')
+Server.create(name: 'Engineer's Server - EST', address: '75.64.227.42:197', map: 'mvm_hightower', password: 'password1234', dedicated: 'true', game: 'highlander', players: '17', maxplayers '18', region: 'EST')
+Server.create(name: 'Bob's Server - EST', address: '143.123.178.194:185', map: 'ctf_idle', password: 'passw0rd', dedicated: 'true', game: 'unspecified', players: '12', maxplayers '17', region: 'EST')
+Server.create(name: 'Carl's RP Server - CST', address: '54.71.62.58:89', map: 'mvm_steel', password: 'pessword', dedicated: 'true', game: 'unspecified', players: '15', maxplayers '15', region: 'CST')
+Server.create(name: 'Bob's RP Server - EST', address: '134.248.250.48:159', map: 'pl_turbine', password: 'pesswerd', dedicated: 'false', game: 'unspecified', players: '9', maxplayers '19', region: 'EST')
+Server.create(name: 'Carl's Prophunt Server - MST', address: '24.165.234.185:183', map: 'ph_steel', password: 'password', dedicated: 'true', game: 'unspecified', players: '19', maxplayers '29', region: 'MST')
+Server.create(name: 'Super Fun Prophunt Server - MST', address: '233.7.37.171:89', map: 'achievement_hightower', password: 'pa55vort', dedicated: 'true', game: 'highlander', players: '17', maxplayers '18', region: 'MST')
+Server.create(name: 'MLG Pro Only Server - PST', address: '96.32.105.199:254', map: 'cp_de_dust', password: 'pa55werd', dedicated: 'false', game: 'unspecified', players: '5', maxplayers '10', region: 'PST')
+Server.create(name: '24/7 2fort Server - PST', address: '162.70.80.221:254', map: 'cp_de_dust', password: 'password', dedicated: 'true', game: 'highlander', players: '9', maxplayers '18', region: 'PST')
+Server.create(name: 'MLG Pro Only RP Server - EST', address: '80.30.46.98:17', map: 'ctf_2fort', password: 'paswerd', dedicated: 'true', game: 'unspecified', players: '13', maxplayers '26', region: 'EST')
+Server.create(name: 'MLG Pro Only Server - MST', address: '231.201.161.85:30', map: 'arena_steel', password: 'possvort', dedicated: 'false', game: 'unspecified', players: '14', maxplayers '26', region: 'MST')
+Server.create(name: '24/7 2fort RP Server - CST', address: '251.5.248.28:31', map: 'arena_turbine', password: 'pasweird', dedicated: 'true', game: 'unspecified', players: '20', maxplayers '24', region: 'CST')
+Server.create(name: 'Bob's Server - PST', address: '200.169.3.121:1', map: 'mvm_turbine', password: 'pessphrase', dedicated: 'false', game: 'unspecified', players: '5', maxplayers '18', region: 'PST')
+Server.create(name: 'Carl's Prophunt Server - MST', address: '26.24.243.59:115', map: 'arena_de_dust', password: 'paswort', dedicated: 'false', game: 'unspecified', players: '11', maxplayers '23', region: 'MST')
+
+
+# USERS 
+
+User.create(username: 'admin1', email: 'cressel@andover.edu', steam_link: 'clyphord', region: 'EST', admin: true, password: 'password');
 User.create!(username: 'admin2', email: 'tnemec@andover.edu', steam_link: 'something', region: 'EST', admin: true, password: 'password');
+User.create!(username: 'claire', email: 'claire73@gmail.com', steam_link: 'claire420', region: 'EST', admin: false, password: 'password');
+User.create!(username: 'heavy', email: 'heavy9@spam.com', steam_link: 'heavy420', region: 'CST', admin: false, password: 'password');
+User.create!(username: 'mr_palfrey', email: 'mr_palfrey82@valvesoftware.net', steam_link: 'mr_palfrey', region: 'PST', admin: false, password: 'password');
+
